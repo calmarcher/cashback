6507fc6d519b096af406e69213ab61375cc37598
Георгий <filonenko.ga@gazprom-neft.ru>
2026-03-02 14:33:40 +0300

    init

---
+++ src/main/java/com/example/cashBack/CashBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.cashBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashBackApplication.class, args);
+	}
+
+}
